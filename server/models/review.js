75b2e2a0d323752e4058a862741364af2b187ed3
vin-vu <vince.hvu@gmail.com>
2021-03-30 23:59:56 -0700

    Working on mongodb

---
+++ server/models/review.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	review: {
+		type: String,
+		required: true
+	}
+}, { timestamps: true });
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,46 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Review = require('./models/review');
+
+// express app
+const app = express();
+
+// connect to mongodb
+const dbURI = 'mongodb+srv://vincehvu:Zundoya1!@cluster0.gzx39.mongodb.net/anime-review?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) => app.listen(3001))
+	.catch((err) => console.log(err))
+
+app.get('/add-review', (req, res) => {
+	const review = new Review ({
+		title: 'naruto test2',
+		review: 'I want to be the 7th hokage'
+	});
+	review.save()
+		.then((result) => {
+			res.send(result)
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 app.get('/', (req, res) => {
-	res.send('here')
+	res.send('here me')
 })
 
-app.listen(3001, () => {
-  console.log('running on port 3001');
-});
+// unknown route handler
+app.use((req, res) => res.sendStatus(400));
+
+// global error handler
+app.use((req, res) => {
+	const defaultErr = {
+		log: 'Express error handler caught unknown middleware error',
+		status: 400,
+		message: { err: 'An error has occured' }
+	};
+	const errObj = Object.assign(defaultErr, err);
+	console.log('ERROR: ', errObj.message);
+	return res.status(errObj.status).send(errObj.message);
+})
